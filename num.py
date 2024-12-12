a4d3189ecdff215bf6b6e242bb775429cb049320
VIPABH <a891983.2@gmail.com>
2024-12-13 00:37:41 +0300

    Update num.py

--- num.py
+++ num.py
@@ -89,8 +89,8 @@ def start(message):
 @bot.callback_query_handler(func=lambda call: call.data == "start_game")
 def start_game(call):
     if call.from_user.id in banned_users:
-        bot.reply_to(call.message, "عذرا , انت محظور من استخدام البوت.")
-        bot.reply_to(call.message, "☝️")
+        # bot.reply_to(call.message, "عذرا , انت محظور من استخدام البوت.")
+        # bot.reply_to(call.message, "☝️")
         return
 
     global game_active, number, attempts, active_player_id
