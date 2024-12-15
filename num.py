70ccac13b8450f0f643ba4db6c5987eb41ac7420
VIPABH <a891983.2@gmail.com>
2024-12-16 01:47:39 +0300

    Update num.py

--- num.py
+++ num.py
@@ -114,20 +114,36 @@ def handle_guess(message):
         bot.reply_to(message, "يرجى إدخال رقم صحيح")
 
 @bot.message_handler(func=lambda message: message.text in ['ميم'] or message.text in ['ميمز'])
-# def send_random_file(message):
-#     rl = random.randint(241, 242)
-#     url = f"t.me/iuabh/{rl}"
-#     if url == t.me/iuabh/242:
-# bot.send_photo(message.chat.id, url, caption="😎يسعد مسائك", reply_to_message_id=message.message_id)
-# else:
-# bot.send_video(message.chat.id, url, caption="😎يسعد مسائك", reply_to_message_id=message.message_id)
-@bot.message_handler(func=lambda message: message.text in ['ميم'] or message.text in ['ميمز'])
-
 def send_random_file(message):
-    rl = random.randint(241, 242)
+    rl = random.randint(240, 255)
     url = f"t.me/iuabh/{rl}"
-    
     if url == "t.me/iuabh/242":
+        bot.send_video(message.chat.id, url, caption="😎يسعد مسائك", reply_to_message_id=message.message_id)
+    if url == "t.me/iuabh/243":
+        bot.send_video(message.chat.id, url, caption="😎يسعد مسائك", reply_to_message_id=message.message_id)
+    if url == "t.me/iuabh/244":
+        bot.send_video(message.chat.id, url, caption="😎يسعد مسائك", reply_to_message_id=message.message_id)
+    if url == "t.me/iuabh/245":
+        bot.send_video(message.chat.id, url, caption="😎يسعد مسائك", reply_to_message_id=message.message_id)
+    if url == "t.me/iuabh/246":
+        bot.send_video(message.chat.id, url, caption="😎يسعد مسائك", reply_to_message_id=message.message_id)
+    if url == "t.me/iuabh/247":
+        bot.send_video(message.chat.id, url, caption="😎يسعد مسائك", reply_to_message_id=message.message_id)
+    if url == "t.me/iuabh/248":
+        bot.send_video(message.chat.id, url, caption="😎يسعد مسائك", reply_to_message_id=message.message_id)
+    if url == "t.me/iuabh/249":
+        bot.send_video(message.chat.id, url, caption="😎يسعد مسائك", reply_to_message_id=message.message_id)
+    if url == "t.me/iuabh/250":
+        bot.send_video(message.chat.id, url, caption="😎يسعد مسائك", reply_to_message_id=message.message_id)
+    if url == "t.me/iuabh/251":
+        bot.send_video(message.chat.id, url, caption="😎يسعد مسائك", reply_to_message_id=message.message_id)
+    if url == "t.me/iuabh/252":
+        bot.send_video(message.chat.id, url, caption="😎يسعد مسائك", reply_to_message_id=message.message_id)
+    if url == "t.me/iuabh/253":
+        bot.send_video(message.chat.id, url, caption="😎يسعد مسائك", reply_to_message_id=message.message_id)
+    if url == "t.me/iuabh/254":
+        bot.send_video(message.chat.id, url, caption="😎يسعد مسائك", reply_to_message_id=message.message_id)
+    if url == "t.me/iuabh/255":
         bot.send_video(message.chat.id, url, caption="😎يسعد مسائك", reply_to_message_id=message.message_id)
     else:
         bot.send_photo(message.chat.id, url, caption="😎يسعد مسائك", reply_to_message_id=message.message_id)
