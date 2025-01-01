d3841912104f5554b88d9f2a39fc04d26988dcd7
VIPABH <a891983.2@gmail.com>
2025-01-01 14:46:30 +0300

    Update db.py

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 if not DATABASE_URL:
     raise ValueError("متغير البيئة 'DATABASE_URL' غير موجود. تأكد من تعيينه بشكل صحيح.")
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
 BASE = declarative_base()
 SessionLocal = sessionmaker(bind=engine)
 SESSION = SessionLocal()
