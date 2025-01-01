1c4e07be761cace6ce3360d4b48c005f43535f66
VIPABH <a891983.2@gmail.com>
2025-01-01 14:26:51 +0300

    Create db.py

---
+++ db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+if not DATABASE_URL:
+    raise ValueError("متغير البيئة 'DATABASE_URL' غير موجود. تأكد من تعيينه بشكل صحيح.")
+
+engine = create_engine(DATABASE_URL, echo=True)
+BASE = declarative_base()
+SessionLocal = sessionmaker(bind=engine)
+SESSION = SessionLocal()
