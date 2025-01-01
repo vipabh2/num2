bb5edc5499e2afc3473b49aa4b70e12a914b1d21
VIPABH <a891983.2@gmail.com>
2025-01-02 01:22:30 +0300

    Update code.py

--- code.py
+++ code.py
@@ -698,10 +698,9 @@ def handle_start(message):
         return 
 
     if message.from_user.id in banned_users:
-        sent_message = bot.send_message(message.chat.id, "☝")
+        sent_message = bot.reply_to(message, "☝")        
         time.sleep(3.5)
         bot.edit_message_text(chat_id=sent_message.chat.id, message_id=sent_message.message_id, text="عذرا , انت محظور من استخدام البوت.")
-
         return
 
     bot.reply_to(
@@ -786,10 +785,10 @@ def show_number(message):
 @bot.message_handler(func=lambda message: game_active and message.from_user.id == active_player_id)
 def handle_guess(message):
     if message.from_user.id in banned_users:
-
-        sent_message = bot.reply_to(message, "☝")        
+        sent_message = bot.send_message(message.chat.id, "☝")
         time.sleep(3.5)
         bot.edit_message_text(chat_id=sent_message.chat.id, message_id=sent_message.message_id, text="عذرا , انت محظور من استخدام البوت.")
+        return
     global game_active, number, attempts
     try:
         guess = int(message.text)
