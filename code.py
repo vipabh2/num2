430b3f31475456eefc8797217090a0466bf6f147
VIPABH <a891983.2@gmail.com>
2025-01-02 03:54:07 +0300

    Update code.py

--- code.py
+++ code.py
@@ -19,26 +19,22 @@ first_message = None
 @bot.message_handler(commands=['send'])
 def handle_send(message):
     global delete_messages, first_message
-    delete_messages = True
+    delete_messages = True 
     bot.send_message(message.chat.id, "تم تفعيل حذف أول رسالة من المستخدم 793977288.")
-    first_message = None 
-
+    first_message = None  
 @bot.message_handler(func=lambda message: delete_messages and message.from_user.id == user_id_to_watch)
 def handle_new_message(message):
     global first_message
 
     if not first_message:
-        first_message = message 
+        first_message = message
     else:
         try:
-            bot.delete_message(first_message.chat.id, first_message.message_id) 
+            bot.delete_message(first_message.chat.id, first_message.message_id)
             first_message = None  
+            bot.send_message(message.chat.id, "تم مسح أول رسالة من المستخدم.")
         except Exception as e:
             print(f"حدث خطأ أثناء حذف الرسالة: {e}")
-
-
-
-
 
 
 abh = [
