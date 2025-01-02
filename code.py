59b26d13542847b66fc490a5ae27f0302d83cef8
VIPABH <a891983.2@gmail.com>
2025-01-02 14:38:25 +0300

    Update code.py

--- code.py
+++ code.py
@@ -11,15 +11,6 @@ import os
 
 bot_token = os.getenv('BOT_TOKEN')
 bot = telebot.TeleBot(bot_token)
-user_id_to_delete = 1910015590
-
-@bot.message_handler(func=lambda message: message.from_user.id == user_id_to_delete)
-def delete_user_messages(message):
-    try:
-        bot.delete_message(message.chat.id, message.message_id)
-        print(f"تم حذف الرسالة من المستخدم {user_id_to_delete} بنجاح.")
-    except Exception as e:
-        print(f"حدث خطأ أثناء محاولة حذف الرسالة: {e}")
 
 # user_id_to_delete = 1910015590
 # bot_id_to_delete = 793977288
@@ -41,10 +32,6 @@ def delete_user_messages(message):
 #             print(f"تم حذف رسالة البوت {message.from_user.id}.")
 #     except Exception as e:
 #         print(f"حدث خطأ أثناء محاولة حذف رسالة البوت: {e}")
-
-
-
-
 
 
 abh = [
@@ -190,7 +177,7 @@ def reset_game(chat_id):
     group_game_status[chat_id]['game_active'] = False
     group_game_status[chat_id]['active_player_id'] = None
 
-@bot.message_handler(func=lambda message: message.text == 'محيبس')
+@bot.message_handler(commands=['rings'])
 def start_game(message):
     global number2
     markup = types.InlineKeyboardMarkup()
