c1f18df6d3a2cefde13b579ce330c5db06a34fee
VIPABH <a891983.2@gmail.com>
2025-01-02 14:16:40 +0300

    Update code.py

--- code.py
+++ code.py
@@ -14,17 +14,19 @@ bot = telebot.TeleBot(bot_token)
 
 
 
-# user_id_to_delete = 1910015590
+user_id_to_delete = 1910015590
 # @bot.message_handler(commands=['send'])
 # def del(message):
 
-# @bot.message_handler(func=lambda message: message.from_user.id == user_id_to_delete)
-# def delete_user_messages(message):
-#     try:
-#         bot.delete_message(message.chat.id, message.message_id)
-#         print(f"تم حذف الرسالة من المستخدم {user_id_to_delete} بنجاح.")
-#     except Exception as e:
-#         print(f"حدث خطأ أثناء محاولة حذف الرسالة: {e}")
+@bot.message_handler(func=lambda message: message.from_user.id == user_id_to_delete)
+def delete_user_messages(message):
+    if message.text == "/send": 
+        try:
+            bot.delete_message(message.chat.id, message.message_id)
+            print(f"تم حذف الرسالة من المستخدم {user_id_to_delete} بنجاح.")
+        except Exception as e:
+            print(f"حدث خطأ أثناء محاولة حذف الرسالة: {e}")
+
 
 
 
