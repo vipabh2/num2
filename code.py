0c00dd1ac73b723a05dd1ce7c43dc54fd75d28d1
VIPABH <a891983.2@gmail.com>
2025-01-02 12:06:35 +0300

    Update code.py

--- code.py
+++ code.py
@@ -12,15 +12,33 @@ import os
 bot_token = os.getenv('BOT_TOKEN')
 bot = telebot.TeleBot(bot_token)
 
-user_id_to_delete = 1910015590
 
-@bot.message_handler(func=lambda message: message.from_user.id == user_id_to_delete)
+
+user_id_to_delete = 1910015590
+delete_messages = False 
+first_message = None 
+
+@bot.message_handler(commands=['send'])
+def del_command(message):
+    global delete_messages, first_message
+    delete_messages = True
+    first_message = None 
+    bot.send_message(message.chat.id, "تم تفعيل حذف أول رسالة من المستخدم.")
+
+@bot.message_handler(func=lambda message: delete_messages and message.from_user.id == user_id_to_delete)
 def delete_user_messages(message):
-    try:
-        bot.delete_message(message.chat.id, message.message_id)
-        print(f"تم حذف الرسالة من المستخدم {user_id_to_delete} بنجاح.")
-    except Exception as e:
-        print(f"حدث خطأ أثناء محاولة حذف الرسالة: {e}")
+    global first_message
+    if not first_message:
+        first_message = message
+    else:
+        try:
+    
+            bot.delete_message(first_message.chat.id, first_message.message_id)
+            bot.send_message(message.chat.id, "تم حذف أول رسالة من المستخدم.")
+            first_message = None 
+        except Exception as e:
+            print(f"حدث خطأ أثناء محاولة حذف الرسالة: {e}")
+
 
 
 
