fc5af8071ed06761f4483a873c5a26def9c096bf
VIPABH <a891983.2@gmail.com>
2025-01-06 21:49:52 +0300

    Update code.py

--- code.py
+++ code.py
@@ -35,7 +35,32 @@ async def reply(event):
         await event.reply(file=vipabh)
     else:
         await event.reply(vipabh)
-############################################
+#################################
+@Client.on(events.NewMessage(pattern=r'(ترجمة|ترجمه)'))
+async def handle_message(event):
+    if event.is_reply:
+        replied_message = await event.get_reply_message()
+        original_text = replied_message.text 
+    else:
+        command_parts = event.message.text.split(' ', 1)
+        original_text = command_parts[1] if len(command_parts) > 1 else None
+
+    if not original_text:
+        await event.reply("يرجى الرد على رسالة تحتوي على النص المراد ترجمته أو كتابة النص بجانب الأمر.")
+        return
+
+    detected_language = translator.detect(original_text)
+    if detected_language.lang == "ar": 
+        translated = translator.translate(original_text, dest="en")
+    else: 
+        translated = translator.translate(original_text, dest="ar")
+
+    response = (
+        f"اللغة المكتشفة: {detected_language.lang}\n"
+        f"النص المترجم: `{translated.text}`"
+    )
+    await event.reply(response)
+###############################
 @ABH.on(events.NewMessage(pattern='ابن هاشم'))
 async def reply_abh(event):
     if event.chat_id == 1001968219024:
