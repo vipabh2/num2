c8bcbce614166047baf2ba1e58f50194cbee6461
VIPABH <a891983.2@gmail.com>
2025-01-06 14:58:23 +0300

    Update code.py

--- code.py
+++ code.py
@@ -39,9 +39,11 @@ async def reply(event):
 @ABH.on(events.NewMessage(pattern='ابن هاشم'))
 async def reply_abh(event):
     rl = random.randint(242, 255)
-    url = f"https://t.me/VIPABH/{rl}" 
+    url = f"https://t.me/VIPABH/{rl}"  
     caption = "ابن هاشم (رض) مرات متواضع ،🌚 @K_4x1"
-    await event.reply(f"Here is the link: {url}", caption=caption)
+    print(url)
+    print(caption)
+    await event.reply(f"Here is the link: {url}\n{caption}")
 ##########################################
 url = "https://ar.wikipedia.org/w/api.php"
 searching_state = {}
