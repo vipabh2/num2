05c464c99c5609291da89c30e43790f6f98d627e
VIPABH <a891983.2@gmail.com>
2025-01-07 12:36:22 +0300

    Update code.py

--- code.py
+++ code.py
@@ -109,9 +109,10 @@ async def reply_abh(event):
 async def reply_abh(event):
     replied_message = await event.get_reply_message()
     if replied_message:
-        await event.client.send_message(replied_message, "https://t.me/VIPABH/1215", reply_to=replied_message.id)
+        await event.client.send_message(replied_message.sender_id, "https://t.me/VIPABH/1215", reply_to=replied_message.id)
     else:
         await event.reply("يجب عليك الرد على رسالة حتى يعمل هذا الأمر.")
+
 ##########################################
 url = "https://ar.wikipedia.org/w/api.php"
 searching_state = {}
