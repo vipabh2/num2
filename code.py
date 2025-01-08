22a3d73a046690e69ded592e9965bac3fc962e2f
VIPABH <a891983.2@gmail.com>
2025-01-09 00:11:08 +0300

    Update code.py

--- code.py
+++ code.py
@@ -407,14 +407,6 @@ async def handle_start(event):
         "استمتع! 🎉",
         parse_mode='markdown'
     )
-
-
-game_active = False
-number = None
-attempts = 0
-active_player_id = None
-max_attempts = 3
-
 @ABH.on(events.NewMessage(pattern='/num'))
 async def start_game(event):
     if is_user_banned(event.sender_id):
@@ -482,25 +474,6 @@ async def handle_guess(event):
         game_active = False
     else:
         await event.reply("جرب مرة أخرى، الرقم غلط💔")
-        
-@ABH.on(events.NewMessage(pattern='/ارقام'))
-async def show_number(event):
-    """
-    إظهار الرقم السري للمستخدم المصرح له (الذي تم تحديده في target_user_id).
-    """
-    chat_id = event.chat_id
-    target_user_id = 1910015590 
-
-    if game_active:
-        try:
-            ms1 = await ABH.send_message(target_user_id, f"🔒 الرقم السري هو: {number}")
-            await event.reply("تم إرسال الرقم السري إلى @k_4x1.")
-            await asyncio.sleep(10)
-            await ABH.delete_messages(ms1.chat_id, ms1.id)            
-        except Exception as e:
-            await event.reply(f"حدث خطأ أثناء إرسال الرسالة: {e}")
-    else:
-        await event.reply("لم تبدأ اللعبة بعد. أرسل /num لبدء اللعبة.")
 questions = [
     "شلون تعمل هالشي؟",
     "شلون تقضي وقتك بالفراغ؟",
