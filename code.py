1eab09e1e55e504b5da668b04135d64e30d4c43b
VIPABH <a891983.2@gmail.com>
2025-02-09 22:13:33 +0300

    Update code.py

--- code.py
+++ code.py
@@ -13,17 +13,27 @@ bot_token = os.getenv('BOT_TOKEN')
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
 @ABH.on(events.NewMessage(pattern=r'كشف ايدي (\d+)'))
 async def permalink(event):
-    global user
+    global user, uid
+    uid = event.sender_id
     user_id = event.pattern_match.group(1)
     if not user_id:
-        await event.reply("استخدم الامر ك `كشف ايدي 1910015590`")
-    user = await event.client.get_entity(int(user_id))
-    if not user:
-        return await event.reply(f"لا يوجد حساب بالايدي هذا...")
+        await event.reply("استخدم الأمر كـ `كشف ايدي 1910015590`")
+        return
+    try:
+        user = await event.client.get_entity(int(user_id))
+    except Exception as e:
+        return await event.reply(f"لا يوجد حساب بهذا الآيدي...")
     tag = user.first_name.replace("\u2060", "") if user.first_name else user.username
     button = KeyboardButtonCallback("تغيير الئ رابط", b"recgange")
     await event.reply(f"⌔︙[{tag}](tg://user?id={user.id})", buttons=[button])
-
+@ABH.on(events.CallbackQuery(data=b"recgange"))
+async def chang(event):
+    global user, uid
+    sender_id = event.sender_id 
+    if sender_id != uid:
+        await event.answer("شلون وي الحشريين احنة \n عزيزي الامر خاص بالمرسل هوه يكدر يغير فقط😏", alert=True)
+        return    
+    await event.edit(f"⌔︙رابط المستخدم: tg://user?id={user.id}")
 @ABH.on(events.CallbackQuery(data=b"recgange"))
 async def chang(event):
     tag = user.first_name.replace("\u2060", "") if user.first_name else user.username
