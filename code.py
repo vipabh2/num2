c2b8ebfddf116d751e19c805a5163e45be1c76d3
VIPABH <a891983.2@gmail.com>
2025-02-14 16:36:48 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,8 @@
+from telethon.errors.rpcerrorlist import UserAdminInvalidError, UserNotParticipantError, ParticipantIdInvalidError
+from telethon.tl.types import ChatBannedRights, ChannelParticipantAdmin, ChannelParticipantCreator
+from telethon.tl.functions.channels import EditBannedRequest, GetParticipantRequest
+import requests, os, operator, asyncio, random, uuid, datetime, re
 from playwright.async_api import async_playwright # type: ignore
-import requests, os, operator, asyncio, random, uuid, datetime
 from database import store_whisper, get_whisper #type: ignore
 from telethon.tl.types import KeyboardButtonCallback
 from telethon import TelegramClient, events, Button
@@ -12,6 +15,62 @@ api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN') 
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
+banned_words = [
+    "سب", "كس", "عير", "كسمك", "كسختك", "كس امك", "طيز", "طيزك", "فرخ", "كواد", 
+    "انيجك", "نيجك", "كحبة", "ابن الكحبة", "ابن الكحبه", "تنيج", "اتنيج", "ينيج", 
+    "اينيج", "بربوك", "زب", "طيزها", "عيري", "خرب الله", "العير", "بعيري", "كحبه", 
+    "برابيك", "نيجني", "نيچني", "نودز", "نتلاوط", "لواط", "لوطي", "فروخ", "منيوك", 
+    "مايا", "ماية", "مايه", "بكسمك", "بكسختك", "🍑", "نغل", "نغولة", "نغوله", "ينغل", 
+    "سكسي", "كحاب", "مناويج", "منيوج", "عيورة", "عيورتكم", "انيجة", "انيچة", "انيجه", 
+    "انيچه", "أناج", "اناج", "انيج", "أنيج", "فريخ", "فريخة", "فريخه", "فرخي", 
+    "كسك", "كسه", "كسة", "اكحاب", "أكحاب", "زنا", "كوم بي", "كمبي", "ارقة جاي", 
+    "ارقه جاي", "يموط", "تموط", "موطلي", "اموط", "بورن", "الفرخ", "الفرحْ", "تيز", "كسم"
+]
+def normalize_text(text):
+    text = re.sub(r'[^أ-يa-zA-Z]', '', text)
+    remove_chars = ['پ', 'ڤ', 'هـ', 'چ', 'گ', 'أ', 'إ', 'آ', 'ئ', 'ژ']
+    for char in remove_chars:
+        text = text.replace(char, '')
+    text = text.replace('ـ', '').replace('ى', '')
+    text = re.sub(r'(.)\1+', r'\1', text)
+    return text
+def clean_message(message):
+    words = message.split()
+    cleaned_words = [word if normalize_text(word) not in map(normalize_text, banned_words) else "****" for word in words]
+    return " ".join(cleaned_words)
+@ABH.on(events.NewMessage)
+async def handler(event):
+    if event.is_group and event.sender_id != 1910015590:
+        if event.raw_text.startswith('#'):
+            new_word = event.raw_text[1:].strip()
+            if new_word not in banned_words:
+                banned_words.append(new_word)
+                await event.reply(f"✅ تم إضافة الكلمة '{new_word}' إلى قائمة الكلمات المحظورة!")
+        else:
+            cleaned_text = clean_message(event.raw_text)
+            if cleaned_text != event.raw_text:
+                # await event.respond(f"🔹 تم تعديل الرسالة: {cleaned_text}")
+                user_id = event.sender_id
+                chat = await event.get_chat()
+                try:
+                    participant = await ABH(GetParticipantRequest(chat.id, user_id))
+                    if isinstance(participant.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)):
+                        return
+                except (UserAdminInvalidError, UserNotParticipantError, ParticipantIdInvalidError):
+                    return
+                restrict_rights = ChatBannedRights(
+                    until_date=None,
+                    send_messages=True,
+                    send_media=True,
+                    send_stickers=True,
+                    send_gifs=True,
+                    send_games=True,
+                    send_inline=True,
+                    embed_links=True
+                )
+                await ABH(EditBannedRequest(chat.id, user_id, restrict_rights))
+                await event.delete()
+                await event.reply(f"⤶ المستخدم [{event.sender.first_name}](tg://user?id={event.sender_id}) \n تم تقييده لاستخدامه كلمة محظورة ☠")
 votes = {'button1': 0, 'button2': 0}
 voted_users = set()
 @ABH.on(events.NewMessage(pattern=r'^تصويت\s+(.+)$'))
@@ -1255,4 +1314,4 @@ questions = [
 async def send_random_question(event):
     random_question = random.choice(questions)
     await event.reply(random_question)
-ABH.run_until_disconnected() 
+ABH.run_until_disconnected()
