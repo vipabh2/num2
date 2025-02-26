270fc409e1be38dfa6ddcdd01c89e3c6f55c8aed
VIPABH <a891983.2@gmail.com>
2025-02-26 16:34:42 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
+from telethon.tl.types import InputMessagesFilterDocument, InputMessagesFilterPhotos, InputMessagesFilterUrl
 from telethon.tl.types import ChatBannedRights, ChannelParticipantAdmin, ChannelParticipantCreator
 from telethon.tl.functions.channels import EditBannedRequest, GetParticipantRequest
 import requests, os, operator, asyncio, random, uuid, datetime, re
@@ -18,6 +19,34 @@ api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN') 
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
+excluded_user_ids = [793977288, 1421907917, 7308514832, 6387632922]
+@ABH.on(events.NewMessage(pattern="/delall"))
+async def delete_filtered_messages(event):
+    """حذف الرسائل بناءً على فلاتر محددة."""
+    await event.delete()
+    try:
+        filters = {
+            "الملفات": InputMessagesFilterDocument,
+            "الروابط": InputMessagesFilterUrl,
+            "الصور": InputMessagesFilterPhotos
+        }
+        total_deleted = 0 
+        deleted_counts = {key: 0 for key in filters.keys()}
+        for msg_type, msg_filter in filters.items():
+            async for message in event.client.iter_messages(event.chat_id, filter=msg_filter):
+                if message.sender_id in excluded_user_ids:
+                    continue 
+                if message:
+                    await message.delete()
+                    deleted_counts[msg_type] += 1
+                    total_deleted += 1
+        if total_deleted > 0:
+            details = "\n".join([f"{msg_type}: {count}" for msg_type, count in deleted_counts.items() if count > 0])
+            await event.reply(f"تم حذف {total_deleted} رسالة.\nالتفاصيل:\n{details}")
+        else:
+            await event.reply("لا توجد رسائل تطابق الفلاتر المحددة!")
+    except Exception as e:
+        await event.reply(f"حدث خطأ أثناء الحذف: {str(e)}")
 @ABH.on(events.NewMessage(pattern=r'(?i)مخفي'))
 async def ai(event):
     if (event.is_reply or len(event.text.strip().split()) > 1) and not event.out:
