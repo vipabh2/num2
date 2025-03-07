ef47e2c086401e5b0a1f29baa65c719ffcf14d76
VIPABH <a891983.2@gmail.com>
2025-03-07 15:27:16 +0300

    Update code.py

--- code.py
+++ code.py
@@ -741,34 +741,28 @@ async def reply(event):
         await event.reply(file=vipabh)
     else:
         await event.reply(vipabh)
-translator = Translator()
-
 @ABH.on(events.NewMessage(pattern=r'(ترجمة|ترجمه)'))
 async def handle_message(event):
+    translator = Translator()
     if event.is_reply:
         replied_message = await event.get_reply_message()
-        original_text = replied_message.text
+        original_text = replied_message.text 
     else:
         command_parts = event.message.text.split(' ', 1)
         original_text = command_parts[1] if len(command_parts) > 1 else None
-
     if not original_text:
-        await event.reply("عزيزي ...\n يرجئ كتابة النص مع الامر أو الرد عليه.")
+        await event.reply("يرجى الرد على رسالة تحتوي على النص المراد ترجمته أو كتابة النص بجانب الأمر.")
         return
-
-    try:
-        detected_language = detect(original_text)
-        target_lang = "en" if detected_language == "ar" else "ar"
-        translated = translator.translate(original_text, dest=target_lang)
-
-        if not translated.text.strip():
-            await event.reply("⚠️ لم يتم العثور على ترجمة صالحة.")
-            return
-
-        response = f"**اللغة المكتشفة:** `{detected_language}`\n**النص المترجم:** `{translated.text}`"
-        await event.reply(response)
-    except Exception as e:
-        await event.reply(f"حدث خطأ أثناء الترجمة: {str(e)}")
+    detected_language = translator.detect(original_text)
+    if detected_language.lang == "ar": 
+        translated = translator.translate(original_text, dest="en")
+    else: 
+        translated = translator.translate(original_text, dest="ar")
+    response = (
+        f"اللغة المكتشفة: {detected_language.lang}\n"
+        f"النص المترجم: `{translated.text}`"
+    )
+    await event.reply(response)
 @ABH.on(events.NewMessage(pattern='ابن هاشم'))
 async def reply_abh(event):
     if event.chat_id == -1001968219024:
