ec87bce539cf222f9d8d9b469e72e4b1639bbeee
VIPABH <a891983.2@gmail.com>
2025-04-01 01:28:42 +0300

    Update code.py

--- code.py
+++ code.py
@@ -31,7 +31,7 @@ def save_points(data, filename="points.json"):
     with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 points = load_points()
-def add_points(uid, gid, points_dict, amount=0):
+def add_points(uid, gid, points_dict, amount=1):
     """إضافة نقاط لمستخدم معين داخل مجموعة معينة."""
     uid, gid = str(uid), str(gid)
     if uid not in points_dict:
@@ -807,25 +807,27 @@ questions_and_answers_q = [
 ]
 user_states = {}
 @ABH.on(events.NewMessage(pattern='اسئلة|/quist'))
-async def start(event):
+async def start_question(event):
+    """بدء السؤال العشوائي"""
     user_id = event.sender_id
     question = random.choice(questions_and_answers_q)
     user_states[user_id] = {
         "question": question,
-        "waiting_for_answer": True 
+        "waiting_for_answer": True
     }
     await event.reply(f"{question['question']}")
 @ABH.on(events.NewMessage)
 async def check_answer(event):
+    """التحقق من إجابة المستخدم"""
     user_id = event.sender_id
     user_message = event.text.strip()
     gid = event.chat_id
     if user_id in user_states and user_states[user_id].get("waiting_for_answer"):
         current_question = user_states[user_id].get("question", {})
-        correct_answers = current_question.get('answer', [])        
+        correct_answers = current_question.get('answer', [])
         if user_message in correct_answers:
-            await event.reply(f"هلا هلا طبوا الشيعة 🫡 \n نقاطك ↢ {points[str(user_id)][str(gid)]['points']}")
             add_points(user_id, gid, points, amount=1)
+            await event.reply(f"هلا هلا طبوا الشيعة 🫡 \n نقاطك ↢ {points[str(user_id)][str(gid)]['points']}")
             del user_states[user_id]
         else:
             pass
