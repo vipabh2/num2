7da018962d6c6915575acb259dbe9b93d0d48343
VIPABH <a891983.2@gmail.com>
2025-04-02 03:06:41 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1095,20 +1095,20 @@ auto = [
 @ABH.on(events.NewMessage(pattern=r'^(سلام عليكم|السلام عليكم)$'))
 async def reply_hi(event):
         abh = random.choice(auto)
-        await event.reply(abh)    
+        await event.reply(abh)
 @ABH.on(events.NewMessage(pattern=r'^(مخفي طكة زيج|زيج)$'))
-async def reply_zig(event):
+async def reply_abh(event):
     replied_message = await event.get_reply_message()
-    if replied_message and str(replied_message.sender_id) == "1910015590":
+    if replied_message and replied_message.sender_id == 1910015590:
         await event.reply("عزيزي الغبي ... \n تريدني اعفط للمطور شكلت لربك؟")
         return
-    if replied_message and replied_message.text:
+    if replied_message:
         abh = random.choice([
             'https://t.me/VIPABH/1171',
             'https://t.me/recoursec/7',
             'https://t.me/recoursec/8'
         ])
-        await event.client.send_file(event.chat_id, abh, reply_to=replied_message.id)
+        await event.client.send_file(replied_message.peer_id, abh, reply_to=replied_message.id)
     else:
         await event.reply("عزيزي الفاهي ... \n الامر يعمل بالرد , اذا عدتها وما سويت رد اعفطلك")
 @ABH.on(events.NewMessage(pattern=r'^(ميعرف|مايعرف)$'))
