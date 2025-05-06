bb70a7339a86781db2f6c2bf3133e38a81756d45
VIPABH <a891983.2@gmail.com>
2025-05-06 13:26:28 +0300

    Update code.py

--- code/code.py
+++ code/code.py
@@ -15,6 +15,24 @@ from googletrans import Translator
 from datetime import datetime
 from bs4 import BeautifulSoup
 from faker import Faker
+hint_gid = -1002168230471
+bot = "Anymous"
+wfffp = 1910015590
+now = datetime.now()
+hour = now.strftime("%y\\%m\\%d--%I:%M%p")
+اسم_الملف = "التشغيل.json"
+if not os.path.exists(اسم_الملف):
+    وقت_التشغيل = hour
+    with open(اسم_الملف, "w", encoding="utf-8") as ملف:
+        json.dump(وقت_التشغيل, ملف, ensure_ascii=False, indent=4)
+@ABH.on(events.NewMessage(pattern='وقت التشغيل'))
+async def time_run(event):
+    if event.sender_id == wfffp:
+        with open(اسم_الملف, "r", encoding="utf-8") as ملف:
+            وقت_التشغيل = json.load(ملف)
+        await event.reply(f"وقت التشغيل هو: {وقت_التشغيل}")
+hour = now.strftime("%I:%M %p")
+print(f'anymous is working at {hour} ✓')
 model = genai.GenerativeModel("gemini-1.5-flash")
 rights_translation = {
     "change_info": "تغيير معلومات المجموعة",
