4d05d257b7e3a6e7a52744c1c617d827d58c73b8
VIPABH <a891983.2@gmail.com>
2025-05-06 13:19:17 +0300

    Update database.py

--- code/database.py
+++ code/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import UniqueConstraint
 
 DATABASE_URL = "sqlite:///db.sqlite3"
 engine = create_engine(DATABASE_URL, echo=False, connect_args={"check_same_thread": False})
