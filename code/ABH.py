e0d1fd330856c399d36b07666accf159472eddc1
VIPABH <a891983.2@gmail.com>
2025-05-06 13:23:26 +0300

    Update ABH.py

--- code/ABH.py
+++ code/ABH.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///db.sqlite3"
