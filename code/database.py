0cdf9c0c5b0cc4b68663f60c9260a928b96026ed
VIPABH <a891983.2@gmail.com>
2025-05-06 13:16:21 +0300

    Update database.py

--- code/database.py
+++ code/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///db.sqlite3"
-engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine(DATABASE_URL, echo=True)
 Base = declarative_base()
 
 class Whisper(Base):
