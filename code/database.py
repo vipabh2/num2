2e7bfa5d6a09bf23d9d8c4f2c84d5446c5b99c8f
VIPABH <a891983.2@gmail.com>
2025-05-06 13:22:25 +0300

    Update database.py

--- code/database.py
+++ code/database.py
@@ -39,31 +39,6 @@ def store_whisper(whisper_id, sender_id, reciver_id, username, message):
 
 def get_whisper(whisper_id):
     return session.query(Whisper).filter_by(whisper_id=whisper_id).first()
-
-def add_approved_user(user_id, group_id):
-    existing_user = session.query(ApprovedUser).filter_by(user_id=user_id, group_id=group_id).first()
-    if not existing_user:
-        new_user = ApprovedUser(user_id=user_id, group_id=group_id)
-        session.add(new_user)
-        session.commit()
-
-def remove_approved_user(user_id, group_id):
-    user_to_remove = session.query(ApprovedUser).filter_by(user_id=user_id, group_id=group_id).first()
-    if user_to_remove:
-        session.delete(user_to_remove)
-        session.commit()
-
-def is_approved_user(user_id, group_id):
-    user = session.query(ApprovedUser).filter_by(user_id=user_id, group_id=group_id).first()
-    return user is not None
-
-def get_approved_users(group_id):
-    users = session.query(ApprovedUser).filter_by(group_id=group_id).all()
-    user_list = []
-    for user in users:
-        user_list.append((user.user_id, user.group_id))
-    return user_list
-
 def init_db():
     Base.metadata.create_all(bind=engine)
 
