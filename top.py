a8ee5eff4cc591387fc47230f17b7ddc2670d099
VIPABH <a891983.2@gmail.com>
2025-05-08 01:42:38 +0300

    Update top.py

--- top.py
+++ top.py
@@ -1,10 +1,9 @@
-from code import *
 from config import *
 @ABH.on(events.NewMessage(pattern=r'^اضف فلوس (\d+)$'))
 async def add_money(event):
     uid = event.sender_id
     r = await event.get_reply_message()
-    if uid == wfffp:
+    if uid == 1910015590:
         p = int(event.pattern_match.group(1))
         gid = event.chat_id
         user_id = r.sender_id
