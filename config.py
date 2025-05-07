51bf6dff62108e5d9a17405aa393bdf1d83b71bf
VIPABH <a891983.2@gmail.com>
2025-05-08 01:55:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-import os
 from telethon import TelegramClient
 from code import *
 from top import *
 from ABH import *
+import os
 def main():
-    print("✅ Bot is starting...")
+    print("config is starting...")
     ABH.start(bot_token=bot_token)
     ABH.run_until_disconnected()
 if __name__ == "__main__":
