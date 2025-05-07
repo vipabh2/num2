d10514f00d1055d0af21460c4bf631fc0a023881
VIPABH <a891983.2@gmail.com>
2025-05-08 01:42:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-from telethon import events
+import os
+import asyncio
+from telethon import TelegramClient
 from code import *
 from top import *
-import asyncio
-api_id = os.getenv('API_ID')      
-api_hash = os.getenv('API_HASH')  
-bot_token = os.getenv('BOT_TOKEN')
-ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
+bot_token = os.getenv("BOT_TOKEN")
+ABH = TelegramClient("code", api_id, api_hash).start(bot_token=bot_token)
 async def main():
-    await ABH.start()
-    print("config is running")
+    print("✅ Bot is running as a bot account (not userbot).")
     await ABH.run_until_disconnected()
 asyncio.run(main())
