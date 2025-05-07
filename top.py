d7350a349f5a17c4a08a27ec193a43e4fc034381
VIPABH <a891983.2@gmail.com>
2025-05-08 00:42:59 +0300

    Update top.py

--- top.py
+++ top.py
@@ -1,10 +1,1 @@
-from code import ABH, events, wfffp, add_points, points
-@ABH.on(events.NewMessage(pattern=r'^اضف فلوس (\d+)$'))
-async def add_money(event):
-    uid = event.sender_id
-    r = await event.get_reply_message()
-    if uid == wfffp:
-        p = int(event.pattern_match.group(1))
-        gid = event.chat_id
-        user_id = r.sender_id
-        add_points(user_id, gid, points, amount=p)
+
