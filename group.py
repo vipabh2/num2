8aa9d0e30d504418ce580687d6b9ce5e386a7311
VIPABH <a891983.2@gmail.com>
2025-05-08 20:38:25 +0300

    Update group.py

--- group.py
+++ group.py
@@ -1,8 +1,33 @@
 from telethon.tl.functions.channels import  GetParticipantRequest
+from telethon.tl.types import KeyboardButtonCallback
 import google.generativeai as genai
 from ABH import ABH, events #type: ignore
 import  pytz
 from googletrans import Translator
+@ABH.on(events.NewMessage(pattern=r'كشف ايدي (\d+)'))
+async def link(event):
+    global user, uid
+    uid = event.sender_id
+    user_id = event.pattern_match.group(1)
+    if not user_id:
+        await event.reply("استخدم الأمر كـ `كشف ايدي 1910015590`")
+        return
+    try:
+        user = await event.client.get_entity(int(user_id))
+    except Exception as e:
+        return await event.reply(f"لا يوجد حساب بهذا الآيدي...")
+    tag = user.first_name.replace("\u2060", "") if user.first_name else user.username
+    button = KeyboardButtonCallback("تغيير الئ رابط", b"recgange")
+    await event.reply(f"⌔︙[{tag}](tg://user?id={user.id})", buttons=[button])
+@ABH.on(events.CallbackQuery(data=b"recgange"))
+async def chang(event):
+    global user, uid
+    sender_id = event.sender_id 
+    if sender_id != uid:
+        await event.answer("شلون وي الحشريين احنة \n عزيزي الامر خاص بالمرسل هوه يكدر يغير فقط😏", alert=True)
+        return
+    if uid is not None and sender_id == uid:
+        await event.edit(f"⌔︙رابط المستخدم: tg://user?id={user.id}")
 timezone = pytz.timezone('Asia/Baghdad')
 GEMINI = "AIzaSyA5pzOpKVcMGm6Aek82KoB3Pk94dYg3LX4"
 genai.configure(api_key=GEMINI)
