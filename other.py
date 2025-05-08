d932f508c170cbeb39abef1ea209cdc8177a4677
VIPABH <a891983.2@gmail.com>
2025-05-08 20:52:01 +0300

    Update other.py

--- other.py
+++ other.py
@@ -1,7 +1,28 @@
 from database import store_whisper, get_whisper #type: ignore
 from playwright.async_api import async_playwright 
+import asyncio, os, json, random, uuid, operator
 from ABH import ABH, events, Button
-import asyncio, os, json, random, uuid
+operations = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.truediv
+}
+@ABH.on(events.NewMessage(pattern=r'احسب (\d+)\s*([\+\-\*/÷])\s*(\d+)'))
+async def calc(event):
+    try:
+        match = event.pattern_match 
+        a = int(match.group(1))
+        mark = match.group(2)
+        b = int(match.group(3))
+        if mark in operations:
+            result = operations[mark](a, b)
+            await event.respond(f"النتيجة `{result}`", reply_to=event.message.id)
+        else:
+            await event.respond("عملية غير مدعومة!", reply_to=event.message.id)
+    except ZeroDivisionError:
+        await event.respond("خطأ: لا يمكن القسمة على صفر!", reply_to=event.message.id)
+
 @ABH.on(events.NewMessage(pattern='ميم|ميمز'))
 async def meme(event):
     global c
