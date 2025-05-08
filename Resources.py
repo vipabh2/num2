d8cae9b5e0fafff6ba331a394da474b91f9cb5e1
VIPABH <a891983.2@gmail.com>
2025-05-08 20:02:43 +0300

    Update Resources.py

--- Resources.py
+++ Resources.py
@@ -1,2 +1,9 @@
 from ABH import *
+timezone = pytz.timezone('Asia/Baghdad')
+GEMINI = "AIzaSyA5pzOpKVcMGm6Aek82KoB3Pk94dYg3LX4"
+genai.configure(api_key=GEMINI)
+model = genai.GenerativeModel("gemini-1.5-flash")
 group = -1001784332159
+hint_gid = -1002168230471
+bot = "Anymous"
+wfffp = 1910015590
