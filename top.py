438466cd6e477686e0c5f82a654b5a023131728a
VIPABH <a891983.2@gmail.com>
2025-05-08 20:07:26 +0300

    Update top.py

--- top.py
+++ top.py
@@ -1,4 +1,36 @@
 from config import *
+import json
+from ABH import ABH, events
+def load_points(filename="points.json"):
+    try:
+        with open(filename, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+def save_points(data, filename="points.json"):
+    with open(filename, "w") as file:
+        json.dump(data, file, indent=4)
+points = load_points()
+def add_points(uid, gid, points_dict, amount=0):
+    uid, gid = str(uid), str(gid)
+    if uid not in points_dict:
+        points_dict[uid] = {}
+    if gid not in points_dict[uid]:
+        points_dict[uid][gid] = {"points": 0}
+    points_dict[uid][gid]["points"] += amount
+    save_points(points_dict)
+def add_user(uid, gid, name, rose, amount):
+    uid, gid = str(uid), str(gid)
+    if gid not in rose:
+        rose[gid] = {}
+    if uid not in rose[gid]:
+        rose[gid][uid] = {
+            "name": name,
+            "status": "عادي",
+            "giver": None,
+            "m": amount,
+            "promote_value": 0
+        }
 @ABH.on(events.NewMessage(pattern=r'^اضف فلوس (\d+)$'))
 async def add_money(event):
     uid = event.sender_id
