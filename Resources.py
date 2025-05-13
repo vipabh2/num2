1bc125d85e06a9405783efd4f6b0b711b8c8d314
VIPABH <a891983.2@gmail.com>
2025-05-14 00:27:36 +0300

    Update Resources.py

--- Resources.py
+++ Resources.py
@@ -9,3 +9,205 @@ group = -1001784332159
 hint_gid = -1002168230471
 bot = "Anymous"
 wfffp = 1910015590
+football = [
+        {
+            "answer": "الميعوف",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/c/2219196756/21013"
+        },
+        {
+            "answer": "سالم الدوسري",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/54"
+        },
+        {
+            "answer": "العويس",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/56"
+        },
+        {
+            "answer": "علي البليهي",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/58"
+        },
+        {
+            "answer": "جحفلي",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/60"
+        },
+        {
+            "answer": "الشلهوب",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/62"
+        },
+        {
+            "answer": "محمد البريك",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/64"
+        },
+        {
+            "answer": "سعود",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/66"
+        },
+        {
+            "answer": "ياسر الشهراني",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/70"
+        },
+        {
+            "answer": "كريستيانو رونالدو",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/72"
+        },
+        {
+            "answer": "امبابي",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/74"
+        },
+        {
+            "answer": "مودريتش",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/76"
+        },
+        {
+            "answer": "بنزيما",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/78"
+        },
+        {
+            "answer": "نيمار",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/80"
+        },
+        {
+            "answer": "ميسي",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/82"
+        },
+        {
+            "answer": "راموس",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/84"
+        },
+        {
+            "answer": "اشرف حكيمي",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/86"
+        },
+        {
+            "answer": "ماركينيوس",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/88"
+        },
+        {
+            "answer": "محمد صلاح",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/90"
+        },
+        {
+            "answer": "هازارد",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/92"
+        },
+        {
+            "answer": "مالديني",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/94"
+        },
+        {
+            "answer": "انيستا",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/96"
+        },
+        {
+            "answer": "تشافي",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/98"
+        },
+        {
+            "answer": "بيكيه",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/100"
+        },
+        {
+            "answer": "بيل",
+            "caption": "شنو اسم الاعب ؟",
+            "photo": "https://t.me/LANBOT2/102"
+        },
+        {
+            "answer": "1995",
+            "caption": "الصوره هذي في اي عام ؟",
+            "photo": "https://t.me/LANBOT2/104"
+        },
+        {
+            "answer": "1997",
+            "caption": "الصوره هذي في اي عام ؟",
+            "photo": "https://t.me/LANBOT2/106"
+        },
+        {
+            "answer": "1998",
+            "caption": "الصوره هذي في اي عام ؟",
+            "photo": "https://t.me/LANBOT2/108"
+        },
+        {
+            "answer": "1999",
+            "caption": "الصوره هذي في اي عام ؟",
+            "photo": "https://t.me/LANBOT2/110"
+        },
+        {
+            "answer": "2002",
+            "caption": "الصوره هذي في اي عام ؟",
+            "photo": "https://t.me/LANBOT2/112"
+        },
+        {
+            "answer": "2005",
+            "caption": "الصوره هذي في اي عام ؟",
+            "photo": "https://t.me/LANBOT2/114"
+        },
+        {
+            "answer": "2007",
+            "caption": "الصوره هذي في اي عام ؟",
+            "photo": "https://t.me/LANBOT2/116"
+        },
+        {
+            "answer": "2008",
+            "caption": "الصوره هذي في اي عام ؟",
+            "photo": "https://t.me/LANBOT2/118"
+        },
+        {
+            "answer": "2009",
+            "caption": "الصوره هذي في اي عام ؟",
+            "photo": "https://t.me/LANBOT2/120"
+        },
+        {
+            "answer": "2000",
+            "caption": "الصوره هذي في اي عام ؟",
+            "photo": "https://t.me/LANBOT2/122"
+        },
+        {
+            "answer": "انشيلوتي",
+            "caption": "شنو اسم المدرب ؟",
+            "photo": "https://t.me/LANBOT2/124"
+        },
+        {
+            "answer": "مورينيو",
+            "caption": "شنو اسم المدرب ؟",
+            "photo": "https://t.me/LANBOT2/126"
+        },
+        {
+            "answer": "بيب غوارديولا",
+            "caption": "شنو اسم المدرب ؟",
+            "photo": "https://t.me/LANBOT2/128"
+        },
+        {
+            "answer": "هيرفي رينارد",
+            "caption": "شنو اسم المدرب ؟",
+            "photo": "https://t.me/LANBOT2/130"
+        },
+        {
+            "answer": "زيدان",
+            "caption": "شنو اسم المدرب ؟",
+            "photo": "https://t.me/LANBOT2/132"
+        }
+]
