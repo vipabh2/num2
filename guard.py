26a64b3d15016726fd3d88cc3d5429d8b09b56df
VIPABH <a891983.2@gmail.com>
2025-05-17 01:36:09 +0300

    Update guard.py

--- guard.py
+++ guard.py
@@ -104,7 +104,10 @@ async def edited(event):
 
     ايديه ⇠ {uid}
     هل كان هذا تلغيم؟
-    """, Button=b)
+    """,                           
+    buttons=b,
+    link_preview=True
+    )
         await asyncio.sleep(60)
         await event.delete()
 banned_words = [
