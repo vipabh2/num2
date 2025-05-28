b1c034f93846df5c25ade19456b8f502dcec1b9e
VIPABH <a891983.2@gmail.com>
2025-05-29 00:53:51 +0300

    Update count.py

--- count.py
+++ count.py
@@ -2,6 +2,14 @@ from ABH import ABH #type: ignore
 from datetime import datetime
 from telethon import events
 import asyncio, os, json
+def load_data():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    return {}
+def save_data(data):
+    with open(DATA_FILE, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
 DATA_FILE = "uinfo.json"
 DATA_FILE_WEAK = "uinfoWEAK.json"
 RESET_FILE = "last_reset.txt"
