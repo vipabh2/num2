ce3f249007f32a187980adac0be3bd72b39c84a8
VIPABH <a891983.2@gmail.com>
2025-06-02 00:48:04 +0300

    Update other.py

--- other.py
+++ other.py
@@ -1,5 +1,5 @@
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
-import asyncio, os, json, random, uuid, operator, requests, aiohttp
+import asyncio, os, json, random, uuid, operator, requests, aiohttp, re
 from telethon.tl.functions.channels import GetParticipantRequest
 from database import store_whisper, get_whisper #type: ignore
 from Resources import CHANNEL, suras, mention #type: ignore
