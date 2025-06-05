0b24898e550d10d2f8e3f4dfd26a2e9751b4b9c3
VIPABH <a891983.2@gmail.com>
2025-06-05 12:11:06 +0300

    Update other.py

--- other.py
+++ other.py
@@ -16,9 +16,9 @@ async def creat_useFILE():
             json.dump({}, f, ensure_ascii=False, indent=4)
 async def botuse(event):
     await creat_useFILE()
-    if event.pattern_match:
-        x = event.pattern_match.group(0)
-    else:
+    # if event.pattern_match:
+    #     x = event.pattern_match.group(0)
+    # else:
         inspect.currentframe().f_code.co_name
     data = {}
     with open('use.py', 'r', encoding='utf-8') as f:
