8a6421936919c7a2988df152f25a0fe123173ef3
VIPABH <a891983.2@gmail.com>
2025-06-05 11:43:30 +0300

    Update Resources.py

--- Resources.py
+++ Resources.py
@@ -1,6 +1,5 @@
 
 import google.generativeai as genai
-from other import botuse
 from ABH import *
 import pytz
 timezone = pytz.timezone('Asia/Baghdad')
@@ -12,7 +11,6 @@ hint_gid = -1002168230471
 bot = "Anymous"
 wfffp = 1910015590
 async def mention(event):
-        await botuse(event)
         sender = await event.get_sender()
         name = sender.first_name or 'name'
         id = sender.id
