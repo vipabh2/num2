819950f650f45ff3c2903e55a3005ad5527ad43f
VIPABH <a891983.2@gmail.com>
2025-06-05 11:35:27 +0300

    Update mem.py

--- mem.py
+++ mem.py
@@ -1,7 +1,9 @@
 from ABH import ABH, events
+from other import botuse
 import random
 @ABH.on(events.NewMessage(pattern=r'^(مخفي طكة زيج|زيج|مخفي اعفطلة)$'))
 async def reply_abh(event):
+    await botuse(event)
     replied_message = await event.get_reply_message()
     if replied_message and replied_message.sender_id == 1910015590:
         await event.reply("عزيزي الغبي ... \n تريدني اعفط للمطور شكلت لربك؟")
@@ -17,6 +19,7 @@ async def reply_abh(event):
         await event.reply("عزيزي الفاهي ... \n الامر يعمل بالرد , اذا عدتها وما سويت رد اعفطلك")
 @ABH.on(events.NewMessage(pattern=r'^(ميعرف|مايعرف)$'))
 async def reply_mem(event):
+    await botuse(event)
     replied_message = await event.get_reply_message()
     if replied_message:
         await event.client.send_file(replied_message.peer_id, "https://t.me/recoursec/3", reply_to=replied_message)
@@ -24,6 +27,7 @@ async def reply_mem(event):
         await event.reply(file="https://t.me/recoursec/3", reply_to=event.message.id)
 @ABH.on(events.NewMessage(pattern=r'^(صباح النور|صباح الخير)$'))
 async def reply_mem(event):
+    await botuse(event)
     replied_message = await event.get_reply_message()
     if replied_message:
         await event.client.send_file(replied_message.peer_id, "https://t.me/recoursec/4", reply_to=replied_message)
@@ -31,6 +35,7 @@ async def reply_mem(event):
         await event.reply(file="https://t.me/recoursec/4", reply_to=event.message.id)
 @ABH.on(events.NewMessage(pattern=r'^(لا تتمادة|لا تتماده|تتماده)$'))
 async def reply_mem(event):
+    await botuse(event)
     replied_message = await event.get_reply_message()
     if replied_message:
         await event.client.send_file(replied_message.peer_id, "https://t.me/recoursec/5", reply_to=replied_message)
@@ -38,6 +43,7 @@ async def reply_mem(event):
         await event.reply(file="https://t.me/recoursec/5", reply_to=event.message.id)
 @ABH.on(events.NewMessage(pattern=r'^(هيه حسب|هاي بعد|اي هاي)$'))
 async def reply_mem(event):
+    await botuse(event)
     replied_message = await event.get_reply_message()
     if replied_message:
         await event.client.send_file(replied_message.peer_id, "https://t.me/recoursec/6", reply_to=replied_message)
@@ -45,6 +51,7 @@ async def reply_mem(event):
         await event.reply(file="https://t.me/recoursec/6", reply_to=event.message.id)
 @ABH.on(events.NewMessage(pattern=r'^(يله شنسوي|ههههه)$'))
 async def reply_mem(event):
+    await botuse(event)
     replied_message = await event.get_reply_message()
     if replied_message:
         await event.client.send_file(replied_message.peer_id, "https://t.me/recoursec/9", reply_to=replied_message)
@@ -52,6 +59,7 @@ async def reply_mem(event):
         await event.reply(file="https://t.me/recoursec/9", reply_to=event.message.id)
 @ABH.on(events.NewMessage(pattern=r'^(man up|استرجل)$'))
 async def reply_mem(event):
+    await botuse(event)
     replied_message = await event.get_reply_message()
     if replied_message:
         await event.client.send_file(replied_message.peer_id, "https://t.me/recoursec/10", reply_to=replied_message)
