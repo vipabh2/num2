e03ede9bfc858eb599413e939f46e43b4c57b951
VIPABH <a891983.2@gmail.com>
2025-06-07 15:25:17 +0300

    Update يوت.py

--- يوت.py
+++ يوت.py
@@ -3,6 +3,18 @@ from telethon import events, Button
 from yt_dlp import YoutubeDL
 import os, asyncio, json
 from ABH import ABH
+import os
+import json
+def isc(chat_id: int, key: str) -> bool:
+    data = {}
+    if os.path.exists("locks.json"):
+        with open("locks.json", "r", encoding="utf-8") as f:
+            try:
+                data = json.load(f)
+            except json.JSONDecodeError:
+                return False
+    chat_id_str = str(chat_id)
+    return data.get(chat_id_str, {}).get(key, False)
 COOKIES_FILE = 'c.txt'
 if not os.path.exists("downloads"):
     os.makedirs("downloads")
@@ -29,6 +41,8 @@ YDL_OPTIONS = {
 }
 @ABH.on(events.NewMessage(pattern=r'^(يوت|yt) (.+)'))
 async def download_audio(event):
+    if not isc(event.chat_id, "اليوتيوب"):
+        return
     try:
         query = event.pattern_match.group(2)
         b = Button.url('CHANNEL', 'https://t.me/X04OU')
@@ -111,7 +125,7 @@ async def add_cookie(event):
     with open("cookie.json", "w", encoding="utf-8") as f:
         json.dump({"cookie_data": content}, f, ensure_ascii=False, indent=2)
     await event.reply(" تم حفظ الكوكيز داخل ملف JSON بنجاح.")
-@ABH.on(events.NewMessage(pattern=r'^ال(\w+)\s+(تعطيل|تفعيل)$'))
+@ABH.on(events.NewMessage(pattern=r'^ال(\w+)\s+(تعطيل|تفعيل)$', from_users=[1910015590]))
 async def handle_flag(event):
     key = event.pattern_match.group(1)
     value_str = event.pattern_match.group(2).lower()
@@ -128,4 +142,4 @@ async def handle_flag(event):
     data[event.chat_id][key] = value
     with open("locks.json", "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
-    await event.reply(f"✅ تم تعيين القيمة للعنصر '{key}' إلى {value}")
+    await event.reply(f" تم تعيين القيمة للعنصر '{key}' إلى {value}")
