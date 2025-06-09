9e9860938c75daa84846238c85d410cb52bd6164
VIPABH <a891983.2@gmail.com>
2025-06-09 14:10:51 +0300

    Update Resources.py

--- Resources.py
+++ Resources.py
@@ -13,9 +13,9 @@ async def mention(event):
     name = getattr(event.sender, 'first_name', None) or 'غير معروف'
     user_id = event.sender_id
     return f"[{name}](tg://user?id={user_id})"
-async def ment(user):
-    name = getattr(user, 'first_name', None) or 'غير معروف'
-    user_id = user.id
+async def ment(sender):
+    name = sender.first_name
+    user_id = sender.id
     return f"[{name}](tg://user?id={user_id})"
 football = [
         {
