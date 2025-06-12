c0de25a1cdda67583acd727cd2d7cf207672eab2
VIPABH <a891983.2@gmail.com>
2025-06-12 12:06:35 +0300

    Update Program.py

--- Program.py
+++ Program.py
@@ -50,7 +50,7 @@ def save_users(data):
 @ABH.on(events.NewMessage)
 async def log_user_by_type(event):
     user_id = event.sender_id
-    name = event.first_name or "بدون اسم"
+    name = "بدون اسم"
     username = event.username or "بدون معرف"
     link = f"tg://user?id={event.id}"
     if event.is_private:
