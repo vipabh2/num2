4cdc6a4736cae72308530825590d72b9e55e2ec2
VIPABH <a891983.2@gmail.com>
2025-06-13 13:35:58 +0300

    Update Program.py

--- Program.py
+++ Program.py
@@ -2,20 +2,21 @@ from telethon import events, Button
 from other import wfffp
 import os, json, redis
 from ABH import ABH
-r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 CHANNEL_KEY = 'saved_channel'
+r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 async def chs(event, c):
     buttons = Button.url('🫆', url=f'https://t.me/{CHANNEL_KEY}')
     await ABH.send_message(event.chat_id, c, reply_to=event.id, buttons=buttons)
 @ABH.on(events.NewMessage(pattern=r'^تعيين القناة (.+)', from_users=[wfffp]))
 async def add_channel(event):
+    global CHANNEL_KEY
     ch = event.pattern_match.group(1)
     x = r.exists(CHANNEL_KEY)
     if x:
         r.delete(CHANNEL_KEY)
     r.set(CHANNEL_KEY, ch)
-    CHANNEL_KEY = ch
     await event.reply(f" تم حفظ القناة {ch}")
+    CHANNEL_KEY = ch
 @ABH.on(events.NewMessage(pattern=r'^عرض القناة$', from_users=[wfffp]))
 async def show_channel(event):
     ch = r.get(CHANNEL_KEY)
