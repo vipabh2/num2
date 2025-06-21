2e5bba60373afa1fcde64e9f54bb181b7eb5a0ec
VIPABH <a891983.2@gmail.com>
2025-06-21 14:01:44 +0300

    Update top.py

--- top.py
+++ top.py
@@ -11,14 +11,14 @@ def save_points(data, filename="points.json"):
     with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 points = load_points()
-def add_points(uid, gid, points_dict, amount=0):
+def add_points(uid, gid, points, amount=0):
     uid, gid = str(uid), str(gid)
-    if uid not in points_dict:
-        points_dict[uid] = {}
-    if gid not in points_dict[uid]:
-        points_dict[uid][gid] = {"points": 0}
-    points_dict[uid][gid]["points"] += amount
-    save_points(points_dict)
+    if uid not in points:
+        points[uid] = {}
+    if gid not in points[uid]:
+        points[uid][gid] = {"points": 0}
+    points[uid][gid]["points"] += amount
+    save_points(points)
 def add_user(uid, gid, name, rose, amount):
     uid, gid = str(uid), str(gid)
     if gid not in rose:
