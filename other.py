7c7a876e554b1b75b1c1a6c0fad4644601a62a6b
VIPABH <a891983.2@gmail.com>
2025-07-04 09:24:50 +0300

    Update other.py

--- other.py
+++ other.py
@@ -1,4 +1,3 @@
-
 from telethon.tl.types import ChannelParticipantCreator
 import asyncio, os, json, random, uuid, operator, requests, re
 from telethon.tl.functions.channels import GetParticipantRequest
