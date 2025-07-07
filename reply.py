2dd32099a450ab44ac295f4a2e27677e95ab3279
VIPABH <a891983.2@gmail.com>
2025-07-07 14:13:33 +0300

    Update reply.py

--- reply.py
+++ reply.py
@@ -151,7 +151,6 @@ async def handle_reply(event):
     for key in r.scan_iter(match=pattern):
         reply_name = key.split(":", 2)[-1]
         data = r.hgetall(key)
-        print(data)
         match_type = data.get('match')
         if (
             (match_type == 'exact' and text == reply_name) or
