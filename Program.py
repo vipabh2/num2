8965d994874f48ac14b6383631b359e0ad713623
VIPABH <a891983.2@gmail.com>
2025-07-09 15:21:08 +0300

    Update Program.py

--- Program.py
+++ Program.py
@@ -4,6 +4,11 @@ import json, redis, subprocess
 from Resources import *
 from other import *
 from ABH import ABH
+@ABH.on(events.ChatAction)
+async def on_bot_added(event):
+    if event.user_added or event.user_joined:
+        if event.user_id == (await client.get_me()).id:
+            await event.reply("يالفكر ضفتني عضو دضيفني مشرف شبيك")
 @ABH.on(events.NewMessage(pattern='مخفي اطلع'))
 async def memkikme(event):
     if not event.is_group:
