e7594660e582e7076a603525b794925dc56c9603
VIPABH <a891983.2@gmail.com>
2025-07-26 00:31:09 +0300

    Update other.py

--- other.py
+++ other.py
@@ -235,7 +235,7 @@ async def myname(event):
     await botuse(type)
     name = await mention(event)
     await event.reply(name)
-@ABH.on(events.NewMessage(pattern=r"^اسمه(?:ة)?$"))
+@ABH.on(events.NewMessage(pattern=r"^اسمه|اسمة$"))
 async def hisname(event):
     type = "اسمه"
     await botuse(type)
