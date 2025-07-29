b5956b78d5f538cd618f446082ba234863d56366
VIPABH <a891983.2@gmail.com>
2025-07-30 01:39:16 +0300

    Update addanddel.py

--- addanddel.py
+++ addanddel.py
@@ -156,8 +156,8 @@ async def promoti(event):
         return
     rights = promot[chat_id][target_user_id]['rights']
     if data == 'done':
+        await react(event, "❤")
         await event.answer(' تم تنفيذ الترقية', alert=False)
-        await react(event, "👍")
         await event.edit('تم رفع المستخدم بنجاح \n لتغيير اللقب ارسل ```تغيير لقبي ``` + لقب معين ')
         admin_rights = ChatAdminRights(
             change_info=rights.get('change_info', False),
