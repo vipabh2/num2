22e1f497aa5b43d35768e13bacac52292f1c3ac2
VIPABH <a891983.2@gmail.com>
2025-07-29 12:25:40 +0300

    Update ABH.py

--- ABH.py
+++ ABH.py
@@ -4,21 +4,3 @@ api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("BOT_TOKEN")
 ABH = TelegramClient("code", api_id, api_hash).start(bot_token=bot_token)
-from telethon.tl.types import MessageEntityUrl
-from telethon import events, Button
-from ABH import ABH  # type:ignore
-from Resources import *
-import asyncio
-from telethon import events, types
-from telethon.sync import TelegramClient
-from telethon.tl.functions.messages import SendReactionRequest
-from telethon.tl.types import InputPeerUser, ReactionEmoji
-
-@ABH.on(events.NewMessage(pattern='ها'))
-async def react_to_message(event):
-    await ABH(SendReactionRequest(
-        peer=event.chat_id,
-        msg_id=event.id,
-        reaction=[ReactionEmoji(emoticon='❤️')],
-        big=True
-    ))
