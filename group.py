29b5794d5128689b44120a637b368b57a1a1bcf4
VIPABH <a891983.2@gmail.com>
2025-08-02 00:35:52 +0300

    Update group.py

--- group.py
+++ group.py
@@ -44,14 +44,24 @@ async def handler(event):
         if target_id == sender_id:
             await event.reply("لا يمكنك إزعاج نفسك.")
             return
+        user_points = points[event.sender_id][chat_id]["points"]
+        if user_points < 50*1000:
+            await event.reply("ما عندك الفلوس الكافيه علمود تسوي ولو واحد ازعاج")
+            return
         sessions[sender_id] = {"target": target_id, "stage": "count", "chat": chat_id}
         await event.reply("عدد؟")
         return
     if sender_id in sessions:
         sess = sessions[sender_id]
         if sess["chat"] == chat_id and sess["stage"] == "count":
-            if text.isdigit() and int(text) > 0:
+            if text.isdigit() and int(text) > 50000:
                 sess["count"] = int(text)
+                x = sess["count"] * 50000
+                if user_points < x:
+                    h = user_points // 50000
+                    await event.reply(f"ما عندك الفلوس الكافيه علمود تسوي ازعاج \n تكدر تسوي ب {h} ازعاج")
+                    del sessions[sender_id]
+                    return
                 sess["stage"] = "emoji"
                 await event.reply("الإيموجي؟")
             else:
