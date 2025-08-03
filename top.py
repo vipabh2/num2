671a35bf8ee8cfd820f0dfc220f16cc79aac696f
VIPABH <a891983.2@gmail.com>
2025-08-03 14:47:22 +0300

    Update top.py

--- top.py
+++ top.py
@@ -95,7 +95,7 @@ async def add_money(event):
         user_id = r.sender_id
         delpoints(user_id, gid, points, amount=p)
         await event.reply(f"تم حذف {p} دينار ل {r.sender.first_name}")
-@ABH.on(events.NewMessage(pattern=r'^تصفير فلوس$'))
+@ABH.on(events.NewMessage(pattern=r'^تصفير فلوس $'))
 async def add_money(event):
     if not event.is_group:
         return
