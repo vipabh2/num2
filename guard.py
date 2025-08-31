528930d448c8b70dc9e96e688c59c5718f3dad32
VIPABH <a891983.2@gmail.com>
2025-09-01 01:44:51 +0300

    Update guard.py

--- guard.py
+++ guard.py
@@ -296,7 +296,7 @@ async def edited(event):
     if not uid in whitelist:
         await msg.delete()
         return
-@ABH.on(events.CallbackQuery(data=b'^yes:(\d+)$'))
+@ABH.on(events.CallbackQuery(data=rb'^yes:(\d+)$'))
 async def yes_callback(event):
     try:
         msg = await event.get_message()
@@ -314,7 +314,7 @@ async def yes_callback(event):
         await event.answer(' تم تسجيل المستخدم كملغّم.')
     except Exception as e:
         await hint(e)
-@ABH.on(events.CallbackQuery(data=b'^no:(\d+)$'))
+@ABH.on(events.CallbackQuery(data=rb'^no:(\d+)$'))
 async def no_callback(event):
     try:
         msg = await event.get_message()
@@ -598,6 +598,7 @@ async def my_warnings(event):
     chat_id = event.chat_id
     user_id = event.sender_id
     warnings = load_warns()
+    print(warnings)
     if chat_id not in warnings or user_id not in warnings[chat_id]:
         await event.reply("لا توجد تحذيرات مسجلة لك.")
         return
