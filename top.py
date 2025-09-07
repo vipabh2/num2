e1b68743ea964249bc9209b62a53806cfc67b172
VIPABH <a891983.2@gmail.com>
2025-09-07 15:32:14 +0300

    Update top.py

--- top.py
+++ top.py
@@ -45,8 +45,8 @@ async def show_rich(event):
     if not points:
         await event.reply("لا توجد بيانات ثروة حالياً.")
         return
-    print(sorted_points)
     sorted_points = sorted(points.items(), key=lambda x: x[1], reverse=True)
+    print(sorted_points)
     top_rich = sorted_points[:10]
     message = "أغنى الأشخاص:\n\n"
     for i, (uid, amt) in enumerate(top_rich, start=1):
