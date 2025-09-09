a9c8734dc3f9ae577cd36e97cb761077c266a394
VIPABH <a891983.2@gmail.com>
2025-09-09 22:15:55 +0300

    Update other.py

--- other.py
+++ other.py
@@ -346,7 +346,7 @@ def ask_ai(q):
 async def ai_handler(event):
     user_q = event.pattern_match.group(1)
     x = event.text
-    ignore_phrases = ["مخفي اعفطلة", "مخفي اعفطله", "مخفي قيده", "مخفي قيدة", "مخفي طكة زيج", "مخفي اطلع"]
+    ignore_phrases = ["غادر مخفي", "مخفي اعفطلة", "مخفي اعفطله", "مخفي قيده", "مخفي قيدة", "مخفي طكة زيج", "مخفي اطلع"]
     if not user_q or x in ignore_phrases:
         return
     type = "ai"
