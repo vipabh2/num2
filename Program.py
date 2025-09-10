1cae10d66840847e0505863f4fcd9853854325f4
VIPABH <a891983.2@gmail.com>
2025-09-11 01:23:46 +0300

    Update Program.py

--- Program.py
+++ Program.py
@@ -98,7 +98,7 @@ async def list_secondary_devs(event):
 @ABH.on(events.NewMessage(pattern=r"^ارسل (.+)$"))
 async def send_handler(event):
     x = save(None, filename="secondary_devs.json")
-    if event.sender_id != wfffp or event.sender_id != 6520830528 and (
+    if event.sender_id != wfffp or (
         str(event.chat_id) not in x or str(event.sender_id) not in x[str(event.chat_id)]):
         return
     r = await event.get_reply_message()
