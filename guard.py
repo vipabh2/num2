5493ee3c0b44d502ccca29959d17c605a552dc97
vipabh2 <a8919832@googlemail.com>
2025-09-13 01:30:47 +0300

    Update guard.py

--- guard.py
+++ guard.py
@@ -558,13 +558,12 @@ async def warn_user(event):
         now = int(time.time())
         restriction_end_times.setdefault(event.chat_id, {})[target_id] = now + restriction_duration
     elif w == 3 and not await is_admin(chat_id, user_id):
-        restriction_end_times.setdefault(event.chat_id, {})[target_id] = now + restriction_duration
         rights = ChatBannedRights(
             until_date=now + restriction_duration,
-            send_messages=True
-        )
-        await botuse("تقييد بسبب التحذير")
+            send_messages=True)
         await ABH(EditBannedRequest(channel=chat_id, participant=target_id, banned_rights=rights))
+        await botuse("تقييد بسبب التحذير")
+        restriction_end_times.setdefault(event.chat_id, {})[target_id] = now + restriction_duration
         return
     await botuse("تحذير مستخدمين")
     if lc:
