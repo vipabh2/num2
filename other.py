419d2efe68bf82c4ebad103428e2631eacc71373
vipabh2 <a8919832@googlemail.com>
2025-09-14 20:39:13 +0300

    Update other.py

--- other.py
+++ other.py
@@ -280,7 +280,7 @@ def save_auth(data):
 def is_assistant(chat_id, user_id):
     data = load_auth()
     assistants = data.get(str(chat_id), [])
-    if user_id in assistants:
+    if user_id in data:
         return True
     return False
 async def is_owner(chat_id, user_id):
