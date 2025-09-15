ab774780f2f198a2b76c2a39f533cbc143854485
VIPABH <a891983.2@gmail.com>
2025-09-15 18:24:21 +0300

    Update guard.py

--- guard.py
+++ guard.py
@@ -636,8 +636,4 @@ async def showwarns(e):
             await chs(e, '🤔')
         target_id = r.sender_id
     w = count_warnings(int(target_id), int(chat))
-    print(w)
-    if chat in w and target_id in w[chat]:
-        العدد = w[chat][target_id]
-        await chs(e, f'تحذيراتك ( 3/{العدد} )')
-        return
+    await chs(e, f'تحذيراتك ( 3/{w} )')
