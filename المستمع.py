11d6cb25cbe43235550f7c19b414c828eaf91dc2
VIPABH <a891983.2@gmail.com>
2025-09-17 12:01:25 +0300

    Update المستمع.py

--- المستمع.py
+++ المستمع.py
@@ -1,9 +1,19 @@
 from telethon import events 
 from Resources import *
 from Program import *
+from count import *
 from ABH import *
 @ABH.on(events.NewMessage)
 async def litsin_to_all(e):
   text = e.text
+  await unified_handler(e)
   await som(e)
+  await unified_handler(e)
   # if text == '':
+@ABH.on(events.CallbackQuery)
+async def litson(e):
+  await callback_handler(e)
+  await callbacklist(e)
+@ABH.on(events.InlineQuery)
+async def litsonINLIN(e):
+  await inlineupdate(e)
