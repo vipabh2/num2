5be516521358d0c2b54f8d00b67417f66a64254a
VIPABH <a891983.2@gmail.com>
2025-09-17 15:11:14 +0300

    Update المستمع.py

--- المستمع.py
+++ المستمع.py
@@ -2,17 +2,37 @@ from telethon import events
 from Resources import *
 from Program import *
 from count import *
+from games import *
 from ABH import *
 @ABH.on(events.NewMessage)
 async def litsin_to_all(e):
   text = e.text
   await unified_handler(e)
   await som(e)
+  await unified_handler(e)
+  await check_math_answer(e)
+  await answer_handler(e)
+  await check_quist(e)
+  await check_sport(e)
+  await faster_reult(e)
+  await faster_reult(e)
+  await monitor_messages(e)
   # if text == '':
 @ABH.on(events.CallbackQuery)
 async def litson(e):
   await callback_handler(e)
   await callbacklist(e)
+  data = e.data.decode('utf-8') if isinstance(e.data, bytes) else e.data
+  if data == "new_math":
+    await new_math(e)
+  elif data == 'ignore_math':
+    await ignore_math(e)
+  elif data == 'moneymuch':
+    await show_money(e)
+  elif data=='startGame':
+      await handle_rings(e)
+  elif data=='startx':
+    await start_game(e)
 @ABH.on(events.InlineQuery)
 async def litsonINLIN(e):
   await inlineupdate(e)
